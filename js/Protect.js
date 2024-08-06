a7a33fb897542f9807a69e486194b3ba579a9452
GideonVermeulen <63670713+GideonVermeulen@users.noreply.github.com>
2024-08-06 21:00:24 +0200

    Fixed spelling error in Protect.js

--- js/Protect.js
+++ js/Protect.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     if (localStorage.getItem('isSignedIn') !== 'true') {
         alert('You are not signed in. Redirecting to sign-in page.');
-        window.location.href = 'signin.html'; // Redirect to sign-in page
+        window.location.href = 'signIn.html'; // Redirect to sign-in page
     }
 });
